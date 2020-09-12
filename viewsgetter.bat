364b33b461ece31b789f1ae0fa92c2d7d263368f
xf8b <yesman58194@tutanota.com>
2020-09-12 16:07:07 -0700

    switch back to REM instead of @rem

--- viewsgetter.bat
+++ viewsgetter.bat
@@ -1,20 +1,20 @@
-@rem username
+REM username
 set user=""
 
-@rem for now, this is the login password. However, a PAT may work, havent figured that out yet
+REM for now, this is the login password. However, a PAT may work, havent figured that out yet
 set password=""
 
-@rem dont change the directory unless you change the java programs too
+REM dont change the directory unless you change the java programs too
 set directory=%~dp0
 Pushd %directory%
 
-@rem this gets all the data that has to do with the user's repos, but we dont really care
+REM this gets all the data that has to do with the user's repos, but we dont really care
 curl "https://api.github.com/users/%user%/repos">%cd%\Repos.txt
 
-@rem repo finder extracts the repo names so that we can do something with it
+REM repo finder extracts the repo names so that we can do something with it
 java RepoRefiner
 
-@rem for each repo, we get taffic, and then parse that data
+REM for each repo, we get taffic, and then parse that data
 for /f "delims=" %%x in (Repos.txt) do (
 curl "https://api.github.com/repos/%user%/%%x/traffic/views" -u %user%:%password%>%cd%\%%x.txt
 java GitHubDataParser %%x Views
