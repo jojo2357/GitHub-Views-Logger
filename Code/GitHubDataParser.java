a559df659aa9bee28ef02d36ed250fd8f7c0a9d4
jojo2357 <66704796+jojo2357@users.noreply.github.com>
2020-09-12 15:02:05 -0700

    Rewrote main data parser
    Hopefully its prettier

--- Code/GitHubDataParser.java
+++ Code/GitHubDataParser.java
@@ -10,19 +10,25 @@ public class GitHubDataParser {
 
     public static String CWD = System.getProperty("user.dir") + "\\";
 
-    public static void main(String args[]) {
-        assert (args.length >= 1);
+    public static void main(String args[]/* first argument is project name, second is clones or views */ ) {
+        assert (args.length == 2);
+
         File inputFile = new File(CWD + args[0] + ".txt");
         File outputFile = new File(CWD + "ParsedData\\" + args[1] + "\\" + args[0] + ".csv");
+
+        String alreadyThere = "";// Stores all of the data that is already in the file that we are working in
+                                 // just as a safeguard
+        TimeStamp lastTimeStamp = null;// Last time stamp in the file, dont put any data in before this date
+
+        FileWriter outputWriter = null;
         Scanner inputReader = null;
+
         try {
             inputReader = new Scanner(inputFile);
         } catch (FileNotFoundException e) {
             throw new RuntimeException("Im so bad");
         }
-        TimeStamp lastTimeStamp = null;
-        FileWriter outputWriter = null;
-        String alreadyThere = "";
+
         if (!(outputFile).exists()) {
             try {
                 outputFile.createNewFile();
@@ -30,95 +36,71 @@ public class GitHubDataParser {
                 outputWriter = new FileWriter(outputFile);
                 outputWriter.write("Date, Total, Unique\n");
             } catch (IOException e) {
+                inputReader.close();
+                throw new RuntimeException("Error making new output file");
             }
-        } else {
+        } else {// now that it exists, we are going to take all of the data for safekeeping and
+                // to prevent duping
             Scanner filePreserver = null;
             try {
                 filePreserver = new Scanner(outputFile);
             } catch (FileNotFoundException e) {
+                throw new RuntimeException("Error reading from stored data");
             }
-            do {
-                alreadyThere += filePreserver.nextLine();
-                if (filePreserver.hasNextLine())
-                    alreadyThere += "\n";
-            } while (filePreserver.hasNextLine());
-            lastTimeStamp = getLastTimeStamp(outputFile);
+            String nextLine = filePreserver.nextLine(); // we need to read the first line since no matter what it will
+                                                        // be "Date, Total, Unique
+            alreadyThere += nextLine;
+            while (filePreserver.hasNextLine()) {
+                nextLine = filePreserver.nextLine();
+                alreadyThere += "\n" + nextLine;
+                String[] holder = nextLine.split(",");
+                lastTimeStamp = new TimeStamp(holder[0], holder[1], holder[2]);// we will jsut keep overwriting until we
+                                                                               // stop and that will be the last time
+                                                                               // stamp
+            }
+            filePreserver.close();
         }
-        if (lastTimeStamp == null) {
-            ArrayList<TimeStamp> timestamps = new ArrayList<TimeStamp>();
-            do {
-                String lineIn = inputReader.nextLine();
-                if (lineIn.contains("\"timestamp\"")) {
-                    String views = inputReader.nextLine();
-                    String uniques = inputReader.nextLine();
-                    timestamps.add(new TimeStamp(lineIn, refine(views), refine(uniques)));
-                }
-            } while (inputReader.hasNextLine());
-            Collections.sort(timestamps);
-            try {
-                if (outputWriter == null)
-                    outputWriter = new FileWriter(outputFile);
-                if (!alreadyThere.isEmpty())
-                    outputWriter.append(alreadyThere + "\n");
-                for (TimeStamp ts : timestamps)
-                    outputWriter.append(ts.toString() + "\n");
-            } catch (IOException e) {
-                e.printStackTrace();
+        ArrayList<TimeStamp> timestamps = new ArrayList<TimeStamp>();
+        do {// read every line that we got FROM GITHUB API (Efectively parsing it)
+            String lineIn = inputReader.nextLine();
+            if (lineIn.contains("\"timestamp\"")) {// if it has ""timestamp":"
+                String views = inputReader.nextLine();// the format of the jsons say that the next line WILL be
+                                                      // views/clones
+                String uniques = inputReader.nextLine();// and the line after that in unique clones/views
+                timestamps.add(new TimeStamp(lineIn, refine(views), refine(uniques)));// create timestamp and add to
+                                                                                      // list
             }
-        } else {
-            ArrayList<TimeStamp> timestamps = new ArrayList<TimeStamp>();
-            do {
-                String lineIn = inputReader.nextLine();
-                if (lineIn.contains("\"timestamp\"")) {
-                    String views = inputReader.nextLine();
-                    String uniques = inputReader.nextLine();
-                    timestamps.add(new TimeStamp(lineIn, refine(views), refine(uniques)));
+        } while (inputReader.hasNextLine());
+        Collections.sort(timestamps);// sort timestamps long ago > now
+        try {
+            if (outputWriter == null)
+                outputWriter = new FileWriter(outputFile);
+            if (!alreadyThere.isEmpty())
+                outputWriter.append(alreadyThere + "\n");
+            if (lastTimeStamp == null) {
+                for (TimeStamp ts : timestamps) {
+                    outputWriter.append(ts.toString() + "\n");
                 }
-            } while (inputReader.hasNextLine());
-            Collections.sort(timestamps);
-            try {
-                if (outputWriter == null)
-                    outputWriter = new FileWriter(outputFile);
-                if (!alreadyThere.isEmpty())
-                    outputWriter.append(alreadyThere + "\n");
+            } else {
                 for (TimeStamp ts : timestamps) {
                     if (ts.compareTo(lastTimeStamp) > 0) {
                         outputWriter.append(ts.toString() + "\n");
                     }
                 }
-            } catch (IOException e) {
-
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         try {
             outputWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-	inputReader.close();
-	System.out.println(
-        inputFile.delete());
+        inputReader.close();
+        System.out.println(inputFile.delete());
     }
 
-    private static TimeStamp getLastTimeStamp(File file) {
-        Scanner outputReader = null;
-        try {
-            outputReader = new Scanner(file);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("File not found or smthn");
-        }
-        String lastLine = "";
-        do {
-            lastLine = outputReader.nextLine();
-        } while (outputReader.hasNextLine());
-        outputReader.close();
-        if (lastLine.contentEquals("Date, Total, Unique"))
-            return null;
-        String[] holder = lastLine.split(",");
-        return new TimeStamp(holder[0], holder[1], holder[2]);
-    }
-
-    private static String refine(String in) {
+    private static String refine(String in) {// refines the views/unique views lines since they all are the same format
         String[] holder = in.split(":");
         in = holder[1];
         holder = in.split(",");
